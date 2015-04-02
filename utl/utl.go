efb60d67d1365dd25ebc114fca114ddf18787ac2
Jason Legler <jlegler@gmail.com>
2015-04-02 16:14:47 -0600

    Added some basic required utility functions and associated tests.

--- utl/utl.go
+++ utl/utl.go
@@ -2,3 +2,63 @@
   Utl contains utility fuctions used by the GoFuzz library.
 */
 package utl
+
+import (
+	"regexp"
+	"unicode/utf8"
+)
+
+// DeDuplicate removes duplicates from a string (except for the letter c).
+func DeDuplicate(s string) string {
+	if utf8.RuneCountInString(s) <= 1 {
+		return s
+	}
+	newVal := string(s[0])
+	for _, char := range s {
+		newValLastLetter, _ := utf8.DecodeLastRuneInString(newVal)
+		c, _ := utf8.DecodeLastRuneInString("c")
+		if char == c || char != newValLastLetter {
+			newVal = newVal + string(char)
+		}
+	}
+	return newVal
+}
+
+// Determines if a string is alphabetic or not.
+func IsAlphabetic(s string) bool {
+	isAlphabetic, _ := regexp.MatchString("[\\W0-9]", s)
+	return !isAlphabetic
+}
+
+// Intersect finds the intersection of two strings.
+func Intersect(s1 string, s2 string) string {
+	short := s1
+	long := s2
+	if utf8.RuneCountInString(s1) > utf8.RuneCountInString(s2) {
+		long = s1
+		short = s2
+	}
+	intersect := ""
+	for _, char1 := range short {
+		for _, char2 := range long {
+			lastRune, _ := utf8.DecodeLastRuneInString(intersect)
+			if char1 == char2 && char1 != lastRune {
+				intersect = intersect + string(char1)
+			}
+		}
+	}
+	return intersect
+}
+
+// Tokenize ngram tokenizes the provided string in groups of n.
+func Tokenize(s string, n int) []string {
+	count := utf8.RuneCountInString(s)
+	if n <= 0 || count < n {
+		return make([]string, 0)
+	}
+	tokenized := make([]string, len(s)-(n-1))
+	for i, j := 0, n; j <= count; i, j = i+1, j+1 {
+		tokenized[i] = string(s[i : i+(n)])
+	}
+	return tokenized
+}
---
+++ utl/utl_test.go
@@ -0,0 +1,68 @@
+package utl
+
+import (
+	"testing"
+)
+
+func TestDeDuplicate(t *testing.T) {
+	if DeDuplicate("a") != "a" {
+		t.Errorf("DeDuplicate fails to deduplicate the single letter a.")
+	}
+	if DeDuplicate("c") != "c" {
+		t.Errorf("DeDuplicate fails to deduplicate the single letter c.")
+	}
+	if DeDuplicate("buzz") != "buz" {
+		t.Errorf("DeDuplicate fails to deduplicate the letter z from buzz.")
+	}
+	if DeDuplicate("accept") != "accept" {
+		t.Errorf("DeDuplicate failed to not deduplicate the letter c from accept.")
+	}
+}
+
+func TestIsAlphabetic(t *testing.T) {
+	if IsAlphabetic("abc3") != false {
+		t.Errorf("Fauled to determine that abc3 is not alphabetic.")
+	}
+	if IsAlphabetic("abc") != true {
+		t.Errorf("Fauled to determine that abc is alphabetic.")
+	}
+}
+
+func TestIntersect(t *testing.T) {
+	if Intersect("context", "contentcontent") != "contet" {
+		t.Errorf("Failed to find the intersect of context and contentcontent.")
+	}
+}
+
+func TestTokenize(t *testing.T) {
+	token := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+		"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	if stringArrayNotEqual(Tokenize("abcdefghijklmnopqrstuvwxyz", 1), token) {
+		t.Errorf("Failed to tokenize string by ngram 1.")
+	}
+	token = []string{"ab", "bc", "cd", "de", "ef", "fg", "gh", "hi", "ij",
+		"jk", "kl", "lm", "mn", "no", "op", "pq", "qr", "rs", "st", "tu", "uv",
+		"vw", "wx", "xy", "yz"}
+	if stringArrayNotEqual(Tokenize("abcdefghijklmnopqrstuvwxyz", 2), token) {
+		t.Errorf("Failed to tokenize string by ngram 2.")
+	}
+	token = []string{"abc", "bcd", "cde", "def", "efg", "fgh", "ghi", "hij",
+		"ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr", "qrs", "rst",
+		"stu", "tuv", "uvw", "vwx", "wxy", "xyz"}
+	if stringArrayNotEqual(Tokenize("abcdefghijklmnopqrstuvwxyz", 3), token) {
+		t.Errorf("Failed to tokenize string by ngram 3.")
+	}
+}
+
+// Compares string slices.
+func stringArrayNotEqual(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return true
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return true
+		}
+	}
+	return false
+}
