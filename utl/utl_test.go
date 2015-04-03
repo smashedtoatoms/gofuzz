007822e7a2966f1f31ab85c21eb0c1885902c4c1
Jason Legler <jlegler@gmail.com>
2015-04-03 12:37:38 -0600

    Added intersect example.

--- utl/utl_test.go
+++ utl/utl_test.go
@@ -52,6 +52,11 @@ func TestIntersect(t *testing.T) {
 	}
 }
 
+func ExampleIntersect(t *testing.T) {
+	Intersect("context", "contentcontent")
+	// Output: "contet"
+}
+
 func TestTokenize(t *testing.T) {
 	token := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 		"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
